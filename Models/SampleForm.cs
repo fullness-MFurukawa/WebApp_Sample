acd68c936ad5f5805c826cb8a759ec0b57dbff13
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-06-04 11:28:55 +0900

    リスト3-3 フォームデータを取得するコントローラ

---
+++ Models/SampleForm.cs
@@ -0,0 +1,18 @@
+
+/// <summary>
+/// リスト3-3 
+/// フォームデータを保持するViewModel
+/// </summary>
+public class SampleForm
+{
+    /// <summary>
+    /// 氏名プロパティ
+    /// </summary>
+    /// <value></value>
+    public string? Name { get; set; }
+    /// <summary>
+    /// 年齢プロパティ
+    /// </summary>
+    /// <value></value>
+    public int Age { get; set; }
+}
